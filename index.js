89046a955cb2fc6a355ebceb9cd4ed997bda855c
BaliBalo <balibalo@pause-geek.fr>
2018-03-21 04:11:09 +0000

    Fix require not working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const ts = require('utils/timestamp');
+const ts = require('./utils/timestamp');
 
 require('./server')(io);
 
